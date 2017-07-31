10bcea13e2dba0f2985b4da6cfd1100f62e73716
ngm84 <nathaniel.g.moore@gmail.com>
2017-07-31 16:05:16 -0400

    First assignment: String and List Practice

---
+++ python_fundamentals.py
@@ -0,0 +1,42 @@
+## Assignment: String and List Practice
+
+#Find and Replace
+def findAndReplace():
+	words = "It's thanksgiving day. It's my birthday, too!"
+	print "First occurence of 'day' begins at index", words.find('day')
+	more_words = words.replace('day','month')
+	print more_words
+
+#Min and Max
+def minAndMax(someList):
+	print "Min:", min(someList)
+	print "Max:", max(someList)
+
+x = [2,54,-2,7,12,98]
+minAndMax(x)
+
+#First and Last
+def firstAndLast(myList):
+	first = myList[0]
+	print first
+	last = myList[len(myList) - 1]
+	print last
+	newList = [first, last]
+	print newList
+
+y = x = ["hello",2,54,-2,7,12,98,"world"]
+firstAndLast(y)
+
+#New List
+def newList(thisList):
+	thisList.sort()
+	half = len(thisList) / 2
+	firstHalf = thisList[:half]
+	lastHalf = thisList[half:]
+	lastHalf.insert(0, firstHalf)
+	print lastHalf
+
+
+z = [19,2,54,-2,7,12,98,32,10,-3,6]
+newList(z)
+
