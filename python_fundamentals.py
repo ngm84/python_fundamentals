b9deefc1c748a072c77b0d06b8b67ec4e32185f9
ngm84 <nathaniel.g.moore@gmail.com>
2017-07-31 16:37:40 -0400

    fixed typo in firstAndLast

--- python_fundamentals.py
+++ python_fundamentals.py
@@ -24,7 +24,7 @@ def firstAndLast(myList):
 	newList = [first, last]
 	print newList
 
-y = x = ["hello",2,54,-2,7,12,98,"world"]
+y = ["hello",2,54,-2,7,12,98,"world"]
 firstAndLast(y)
 
 #New List
@@ -42,19 +42,29 @@ newList(z)
 
 ## Assignment: Multiples, Sum, Average
 
+#Multiples Part I
 def multiplesI():
 	for num in range(1, 1000):
 		if num % 2 != 0:
 			print num
+
+#Multiples Part II
+multiplesI()
 
 def multiplesII():
 	for num in range(5, 1000000):
 		if num % 5 == 0:
 			print num
 
+multiplesII()
+
+#Sum List
 def sumList(listy):
 	sum = 0;
+	# looping over the list, getting a *COPY* of each element --
+	# can't use this syntax to actually change values in a list
 	for i in listy:
+		# Testing to make sure i is an int and can be math'd
 		if type(i) == int:
 			sum += i
 	print sum
@@ -62,6 +72,7 @@ def sumList(listy):
 x = [1, 2, 5, 10, 255, 3]
 sumList(x)
 
+#Average List
 def averageList(listo):
 	sum = 0;
 	for i in listo:
